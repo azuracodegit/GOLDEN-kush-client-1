9ee6ee77513e5e72b413bf9f6ebdddaa1788aad4
azuracodegit <contact@azuracode.fr>
2025-04-21 00:33:39 +0200

    jespere

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Search, UserPlus, Plus, Minus, Edit2, Trash2, ShoppingBag, X, Gift } from 'lucide-react';
 import Confetti from 'react-confetti';
 import { supabase } from './supabaseClient';
+import logo from '../public/old-n-kush-logo.png';
 
 interface Sale {
   id: string;
@@ -376,7 +377,7 @@ function App() {
           <div className="flex items-center justify-between">
             <div className="flex items-center space-x-3">
               <img 
-                src="/old-n-kush-logo.png" 
+                src={logo}
                 alt="Gold N Kush Logo" 
                 className="h-40 w-auto"
               />
